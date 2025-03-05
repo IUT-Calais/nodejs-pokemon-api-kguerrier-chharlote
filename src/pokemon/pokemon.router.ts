8b27bb415b1d3b013a8cdd4eb31d82fbe9b1aa8e
Charlotte Germe <charlotte.germe16@gmail.com>
2025-03-05 15:10:19 +0100

    ajout fonction jwt au router pokemon

--- src/pokemon/pokemon.router.ts
+++ src/pokemon/pokemon.router.ts
@@ -1,20 +1,20 @@
-import { Router } from 'express';
-import {getPokemon, getPokemons, createPokemon, updatePokemon, deletePokemon} from './pokemon.router.controller';
+import { Router } from "express";
+import { getPokemon, getPokemons, createPokemon, updatePokemon, deletePokemon } from "./pokemon.router.controller";
+import { verifyJWT } from "../common/jwt.middleware";
+
 export const pokemonRouter = Router();
 
-// Route pour obtenir la liste des pokemons
-pokemonRouter.get('/', getPokemons);
+// Route pour obtenir la liste des pokémons
+pokemonRouter.get("/", getPokemons);
 
-// Route pour obtenir un pokemon avec son id
-pokemonRouter.get('/:pokemonCardId', getPokemon);
+// Route pour obtenir un pokémon par son ID
+pokemonRouter.get("/:pokemonCardId", getPokemon);
 
-// Route pour créer un pokemon
-pokemonRouter.post('/', createPokemon);
+// Route pour créer un pokémon
+pokemonRouter.post("/", verifyJWT, createPokemon);
 
-// Route pour mettre à jour un pokemon
-pokemonRouter.patch('/:pokemonCardId', updatePokemon);
+// Route pour mettre à jour un pokémon
+pokemonRouter.patch("/:pokemonCardId", verifyJWT, updatePokemon);
 
-// Route pour supprimer un pokemon
-pokemonRouter.delete('/:pokemonCardId', deletePokemon);
-
-
+// Route pour supprimer un pokémon
+pokemonRouter.delete("/:pokemonCardId", verifyJWT, deletePokemon);
