52ed608ad837cbbc8abe50ed2e55d1f1942d27a8
Charlotte Germe <charlotte.germe16@gmail.com>
2025-03-05 09:13:43 +0100

    ajout user route + controller (create login)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { pokemonRouter } from './pokemon/pokemon.router';
+import {userRouter} from "./user/user.router";
 
 export const app = express();
 const port = process.env.PORT || 3000;
@@ -14,6 +15,7 @@ export function stopServer() {
 
 // Route de gestion des pokemons
 app.use('/pokemons-cards', pokemonRouter);
+app.use('/users', userRouter);
 
 
 
