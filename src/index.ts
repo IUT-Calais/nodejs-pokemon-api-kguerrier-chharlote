a8d2055e9b044a75f411d153138b21db8b2685d8
Charlotte Germe <charlotte.germe16@gmail.com>
2025-05-12 10:27:37 +0200

    init swagger

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
 import express from 'express';
 import { pokemonRouter } from './pokemon/pokemon.router';
 import {userRouter} from "./user/user.router";
+import yaml from "yaml";
+import fs from "fs";
+import * as path from "node:path";
+import swaggerUi from "swagger-ui-express";
+
+
+
+const file = fs.readFileSync(path.join(__dirname, "./swagger.yaml"), "utf8");
+const swaggerDocument = yaml.parse(file);
 
 export const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export const server = app.listen(port);
 
